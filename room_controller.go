4d77c58c768ff892ec60a75f2a1da40da5dd9b75
Janis Streib <janis@dogcraft.de>
2016-06-09 00:17:15 +0200

    FMT

--- room_controller.go
+++ room_controller.go
@@ -3,15 +3,14 @@ package main
 import "fmt"
 import "github.com/janisstreib/room_controller/motion_sensor"
 
-
 func addInts(x int, y int) int {
-    return x + y
+	return x + y
 }
 
 func main() {
-    fmt.Printf("Hello World\n")
-    m := motion_sensor.NewGPIOMotionSensor(7)
-    for {
-	fmt.Printf("%d\n", m.MotionActive())
-    }
+	fmt.Printf("Hello World\n")
+	m := motion_sensor.NewGPIOMotionSensor(7)
+	for {
+		fmt.Printf("%d\n", m.MotionActive())
+	}
 }
