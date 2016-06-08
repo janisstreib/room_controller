6804aa781a03b09589fe821ea50e1bce5e572f93
Janis Streib <janis@dogcraft.de>
2016-06-09 00:17:06 +0200

    FIX: only build gpio sensor on  arm platfrom + splutted interface

---
+++ motion_sensor/gpio_motion_sensor_arm.go
@@ -0,0 +1,24 @@
+package motion_sensor
+// #cgo LDFLAGS: -lwiringPi
+// #include <wiringPi.h>
+
+import "C"
+
+type GpioMotionSensor struct {
+	pin int
+}
+
+func (g GpioMotionSensor) MotionActive() bool {
+	if C.digitalRead(C.int(g.pin)) == 1 {
+		return true
+	}
+	return false
+}
+
+func NewGPIOMotionSensor(pin int) *GpioMotionSensor {
+	g := new(GpioMotionSensor)
+	g.pin = pin
+	C.wiringPiSetupPhys()
+	C.pinMode(C.int(pin), C.INPUT)
+	return g
+}
--- motion_sensor/motion_sensor.go
+++ motion_sensor/motion_sensor.go
@@ -1,28 +1,5 @@
 package motion_sensor
-// #cgo LDFLAGS: -lwiringPi
-// #include <wiringPi.h>
-import "C"
 
 type MotionSensor interface {
 	MotionActive() bool
-}
-
-type GpioMotionSensor struct {
-	pin int
-
-}
-
-func (g GpioMotionSensor) MotionActive() bool {
-	if C.digitalRead(C.int(g.pin)) == 1 {
-		return true
-	}
-	return false
-}
-
-func NewGPIOMotionSensor(pin int) *GpioMotionSensor {
-	g := new(GpioMotionSensor)
-	g.pin = pin
-	C.wiringPiSetupPhys()
-	C.pinMode(C.int(pin), C.INPUT)
-	return g
 }
